7b0f6282f9a7ffa05c3fe4a134f7fc163630845a
quakk0 <108644616+quakk0@users.noreply.github.com>
2025-07-22 13:41:11 +0800

    idk what im doing

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,32 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import pluginReact from 'eslint-plugin-react';
+import pluginSecurity from 'eslint-plugin-security';
 
-
+/** @type {import('eslint').Linter.Config[]} */
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { security: pluginSecurity },
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:security/recommended',
+    ],
+    rules: {
+      'security/detect-eval-with-expression': 'error',
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+    },
+  },
 ]);
